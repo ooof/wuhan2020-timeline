0df95b89cd9fb6c4e469e139f38d3b2f68006779
Weilei Zeng <240155787@qq.com>
2020-03-12 09:25:28 -0700

    add search page; recover homepage

--- docs/_site/extract_data_to_yaml.py
+++ docs/_site/extract_data_to_yaml.py
@@ -1,5 +1,5 @@
 # extract data from index.md to _data/news.yml
-
+# just file writing, didn't use the yaml package
 
 file_input=open('index.md','r')
 #file_input=open('../时间线TIMELINE.md','r')
@@ -8,13 +8,13 @@ file_output=open('_data/news.yml','w')
 file_input.readline()
 file_input.readline()
 file_input.readline()
-
+file_input.readline()
 
 for line in file_input:
     if line[0:2] == "##":
         file_output.write('\n- date: '+ line[3:]+'  items:\n')
         
-        print('date: '+line)
+        #print('date: '+line)
     elif line[0:1] == "【":
         file_output.write('  - item_name: group\n    content: "'+line[:-1] + '"\n')
         #print('group '+ line)
