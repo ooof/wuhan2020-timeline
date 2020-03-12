78caaa787b20c1c9fd7ed71c7cf347e40d9197d9
Weilei Zeng <240155787@qq.com>
2020-03-12 00:08:37 -0700

    add data.json

--- docs/yaml2json.py
+++ docs/yaml2json.py
@@ -1,5 +1,6 @@
 yaml_file_name='_data/news.yml'
 json_file_name='news.json'
+json_file2_name='data.json'
 
 import yaml
 import json
@@ -14,11 +15,25 @@ with open(yaml_file_name,'r') as yaml_file:
     #print(yaml.dump(item)
 
     
-    with open(json_file_name,'w', encoding = "utf-8") as json_file:
-        json.dump(item, json_file, ensure_ascii=False, indent=2)
+with open(json_file_name,'w', encoding = "utf-8") as json_file:
+    json.dump(item, json_file, ensure_ascii=False, indent=2)
         #json_file.write(json.dumps(item,indent=2))
+
+
+#change format to match
+list2=[]
+for date in item:
+    if date['items'] != None:
+        for news in date['items']:
+            dict2={}
+            dict2['date']=date['date']
+            dict2['content']=news['content']
+            list2.append(dict2)
+with open(json_file2_name,'w', encoding = "utf-8") as json_file2:
+    json.dump(list2, json_file2, ensure_ascii=False, indent=2)               
 
 print('input:'+yaml_file_name)        
 print('output:'+json_file_name)
+print('output:'+json_file2_name)
 
 
